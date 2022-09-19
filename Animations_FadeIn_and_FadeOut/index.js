307bfef2534dbb1602ffe2056d65bd570374a9f3
RyzenMu <muruga13in2022@gmail.com>
2022-09-19 22:27:43 +0530

    Fade In Not working

--- Animations_FadeIn_and_FadeOut/index.js
+++ Animations_FadeIn_and_FadeOut/index.js
@@ -1,1 +1,43 @@
-document.write("Hello From JS file")
+document.write("<h3>Hello From JS file</h3>")
+
+function fadeOut()
+{
+    intervalID = setInterval(hide, 300);
+}
+
+function hide()
+{
+    var img = document.getElementById('flower');
+    opacity = Number(window.getComputedStyle(img).getPropertyValue('opacity'));
+    if (opacity > 0)
+    {
+        opacity = opacity - 0.02;
+        img.style.opacity = opacity;
+        console.log(opacity)
+    }
+    else
+    {
+        clearInterval(intervalID);
+    }
+}
+
+function fadeIn()
+{
+    intervalID = setInterval(show, 300);
+}
+
+function show()
+{
+    var img = document.getElementById('flower');
+    opacity = Number(window.getComputedStyle(img).getPropertyValue('opacity'));
+    if (opacity < 1)
+    {
+        opacity = opacity + 0.02;
+        img.style.opacity = opacity;
+        console.log(opacity)
+    }
+    else
+    {
+        clearInterval(intervalID);
+    }
+}
