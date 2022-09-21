b03153f920dfea06516eb719ba30b1cf2830295f
RyzenMu <muruga13in2022@gmail.com>
2022-09-21 21:51:43 +0530

    fade in and fade out of an image is completed

--- Animations_FadeIn_and_FadeOut/main.js
+++ Animations_FadeIn_and_FadeOut/main.js
@@ -1,1 +1,9 @@
 document.write("Hello World from Java Script")
+
+function fin(){
+    document.getElementById("image-1").style.opacity = 0.9;
+}
+
+function fout(){
+    document.getElementById('image-1').style.opacity = 0.2;
+}
