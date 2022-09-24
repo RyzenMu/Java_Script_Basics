b92bd5d57de31ce21e4cd7ffb26f75a54039f5d4
RyzenMu <muruga13in2022@gmail.com>
2022-09-24 21:45:40 +0530

    zoom in and zoom out completed

---
+++ Animations_ZoomIn_and_ZoomOut/main.js
@@ -0,0 +1,11 @@
+function shrink(){
+    alert('Shrink');
+    document.getElementById('img-giraffee').style.width = "50px"
+
+}
+
+function large(){
+    alert('Large')
+    document.getElementById('img-giraffee').style.width = "500px"
+}
+
